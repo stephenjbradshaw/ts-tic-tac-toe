fd05151751093644109791d237b64d1fee73fd1c
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-30 16:51:13 +0000

    Refactor Board component to use hooks with TS

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,82 +1,79 @@
-import React from "react";
-import Square from "./Square"
+import React, { useState } from "react";
+import Square from "./Square";
 
-type SquareValue = (null | "X" | "O")
+type SquareValue = null | "X" | "O";
 
-interface State  {
-  squares: SquareValue[];
-  xIsNext: boolean;
-}
+const Board: React.FC = () => {
+  // Explicitly define type of squares by passing into useState's generic
+  const [squares, setSquares] = useState<SquareValue[]>(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
-interface Props {}
-
-class Board extends React.Component<Props, State> {
-  state: State = { squares: Array(9).fill(null), xIsNext: true };
-
-  calculateWinner = (squares: SquareValue[]): SquareValue => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+  // Func takes an arg of type SquareValue array, returns value of type SquareValue
+  const calculateWinner = (squares: SquareValue[]): SquareValue => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
     }
-  }
-  return null;
-};
-
-  handleClick = (i: number): void => {
-    const squares = [...this.state.squares];
-    if (this.calculateWinner(squares) || squares[i]) return;
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState((prevState) => ({ xIsNext: !prevState.xIsNext, squares }));
+    return null;
   };
 
-  renderSquare(i: number): JSX.Element {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
+  const handleClick = (i: number): void => {
+    // Return early if game won or square already filled
+    if (calculateWinner(squares || squares[i])) return;
+    const newSquares = [...squares];
+    // Fill the square with X or O, depending on whose turn it is
+    newSquares[i] = xIsNext ? "X" : "O";
+    setXIsNext(!xIsNext);
+    setSquares(newSquares);
+  };
 
-  render() {
-    const winner = this.calculateWinner(this.state.squares);
-    let status: string;
-    if (winner) {
-      status = `Winner: ${winner}`;
-    } else status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+  // Explicitly declares that func returns JSX
+  const renderSquare = (i: number): JSX.Element => {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  };
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  const winner = calculateWinner(squares);
+  let status: string;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else status = `Next player: ${xIsNext ? "X" : "O"}`;
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
 export default Board;
